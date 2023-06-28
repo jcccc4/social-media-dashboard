d971dd144e913eb14d1bef2bfc2eb90e45458f9d
JC <jcsacra4@gmail.com>
2023-06-29 00:50:57 +0800

    Design Button for Dark Mode

--- src/app/utilities/DarkMode.tsx
+++ src/app/utilities/DarkMode.tsx
@@ -6,7 +6,7 @@ function DarkMode() {
       <span>Dark Mode</span>
       <label className="relative inline-flex cursor-pointer">
         <input type="checkbox" value="" className="sr-only peer/dot" />
-        <div className="w-12 h-6 bg-gray-200 rounded-full peer/dot peer-checked/dot:after:translate-x-6 peer-checked/dot:after:bg-red-500 after:absolute after:top-[3px] after:left-[3px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-[18px] after:w-[18px] after:transition-all peer-checked/dot:bg-blue-600"></div>
+        <div className="w-12 h-6 bg-[#AEB3CB] rounded-full peer/dot  peer-checked/dot:bg-gradient-to-r peer-checked/dot:from-[#40DB82]  peer-checked/dot:to-[#388FE7] peer-checked/dot:after:translate-x-6 after:absolute after:top-[3px] after:left-[3px] after:bg-[#F1F3FA] after:border-gray-300 after:border after:rounded-full after:h-[18px] after:w-[18px] after:transition-all peer-checked/dot:bg-blue-600"></div>
       </label>
     </section>
   );
