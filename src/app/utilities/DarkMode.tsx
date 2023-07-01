50b2505117973e4852a746b481ff1e13c6d35438
JC <jcsacra4@gmail.com>
2023-07-01 13:04:12 +0800

    Fix a Dark Mode bug

--- src/app/utilities/DarkMode.tsx
+++ src/app/utilities/DarkMode.tsx
@@ -1,30 +1,33 @@
-"use client"
+"use client";
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 function DarkMode() {
-  const [mounted, setMounted] = useState(false)
+  const [isInitialRender, setIsInitialRender] = useState(true);
   const { theme, setTheme } = useTheme();
-  const toggleDarkMode = () =>{
-    console.log(theme);
-    theme ==='dark'?setTheme('light'):setTheme('dark')
-  }
-
+  const toggleDarkMode = (e: ChangeEvent<HTMLInputElement>): void => {
+    e.target.checked ? setTheme("dark") : setTheme("light");
+  };
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setIsInitialRender(false);
+  }, []);
 
-  if (!mounted) {
-    return null
-  }
-  
   return (
     <section className="flex justify-between">
       <span className="dark:text-[#8C98C6]">Dark Mode</span>
       <label className="relative inline-flex cursor-pointer">
-        <input type="checkbox" value="" className="sr-only peer/dot" onClick={toggleDarkMode}/>
-        <div className="w-12 h-6 bg-[#AEB3CB] rounded-full peer/dot  peer-checked/dot:bg-gradient-to-r peer-checked/dot:from-[#40DB82]  peer-checked/dot:to-[#388FE7] peer-checked/dot:after:translate-x-6 after:absolute after:top-[3px] after:left-[3px] after:bg-[#F1F3FA] after:border-gray-300 after:border after:rounded-full after:h-[18px] after:w-[18px] after:transition-all peer-checked/dot:bg-blue-600"></div>
+        <input
+          type="checkbox"
+          defaultChecked={theme === "dark"}
+          className="sr-only peer/dot"
+          onChange={(e) => toggleDarkMode(e)}
+        />
+        <div
+          className={`${
+            isInitialRender ? "" : " after:transition-all"
+          } w-12 h-6 bg-[#AEB3CB] rounded-full peer/dot  peer-checked/dot:bg-gradient-to-r peer-checked/dot:from-[#40DB82]  peer-checked/dot:to-[#388FE7] peer-checked/dot:after:translate-x-6 after:absolute after:top-[3px] after:left-[3px] after:bg-[#F1F3FA] after:border-gray-300 after:border after:rounded-full after:h-[18px] after:w-[18px] peer-checked/dot:bg-blue-600`}
+        ></div>
       </label>
     </section>
   );
